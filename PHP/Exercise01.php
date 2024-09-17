95a4cbdd7332fbfb0373ffcce66828b06eecb6d6
Alex Li <alexavililee@gmail.com>
2024-09-17 18:43:14 +0200

    Add files via upload

---
+++ PHP/Exercise01.php
@@ -0,0 +1,19 @@
+<?php
+$num1 = 6;
+$num2 = 4;
+echo "Numero 1: ". $num1. "<br>";
+echo "Numero 2: ".$num2. "<br> <br>";
+if($num1 > $num2){
+echo "Numero 1 (".$num1.") es mayor que Numero 2 (".$num2.") ";
+}else{
+
+}
+echo "<br>";
+
+echo "num1 + num2 = ". ($num1+$num2). "<br>";
+echo "num2 - num1 = ". ($num2-$num1). "<br>";
+echo "num2 * num1 = ". ($num2*$num1). "<br>";
+echo "num2 / num1 = ". ($num2/$num1). "<br>";
+
+
+
