e09b7f097770904ead59cdac3ebe232352297c2a
AlexLi727 <alexavililee@gmail.com>
2024-09-25 20:20:01 +0200

    3

--- PHP/ExerciseArrays.php
+++ PHP/ExerciseArrays.php
@@ -1,6 +1,50 @@
 <?php
+$num = 1;
 $dato["nombre"] = "Sara";
 $dato["apellido"] = "Martinez";
 $dato["edad"] = 23;
 $dato["ciudad"] = "Barcelona";
-echo $dato[2];
+foreach($dato as $value){
+    echo "dato $num: $value <br>";
+    $num++;
+}
+echo "<br>";
+
+foreach($dato as $value => $valor){
+    echo "$value: $valor <br>";
+}
+echo "<br>";
+
+$dato["edad"]++;
+$num = 1;
+foreach($dato as $value){
+    echo "dato $num: $value <br>";
+    $num++;
+}
+
+echo "<br>";
+unset($dato["ciudad"]);
+echo var_dump($dato);
+
+echo "<br>";
+$letters = "a, b, c, d, e, f";
+$letters = explode(",", $letters);
+$num = count($letters);
+foreach(array_reverse($letters) as $letras){
+    echo "Letter $num: $letras <br>";
+    $num--;
+}
+
+echo "<br>";
+$nota["Miguel"] = 5;
+$nota["Luis"] = 7;
+$nota["Marta"] = 10;
+$nota["Isabel"] = 8;
+$nota["Aitor"] = 4;
+$nota["Pepe"] = 1;
+asort($nota);
+echo "Notas de los estudiantes: ";
+foreach(array_reverse($nota) as $antonio => $nombre){
+    echo "$antonio: $nombre ";
+}
+
