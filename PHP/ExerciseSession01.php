a29159d9a60369c4575f9d878951b2d9651fdba4
AlexLi727 <alexavililee@gmail.com>
2024-10-01 17:15:47 +0200

    5

--- PHP/ExerciseSession01.php
+++ PHP/ExerciseSession01.php
@@ -8,7 +8,7 @@
 <body>
     <h1>Supermarket management</h1>
     <form method = POST>
-        Worker name: <input type = name>
+        Worker name: <input type = name name = name>
 
         <h2> Choose product</h2>
         <select name = drink>
@@ -17,15 +17,39 @@
         </select>
 
         <h2>Choose quantity</h2>
-        <input type = number>
+        <input type = number name = quantity>
         <br><br>
-        <input type = submit value = add> 
-        <input type = submit value = remove>
+        <input type = submit value = add name = add> 
+        <input type = submit value = remove name = remove>
         <input type = reset value = reset>
     </form>
 </body>
 </html>
 
 <?php
+if(isset($_POST["name"]) && isset($_POST["quantity"])){
+    $name = $_POST["name"];
+    $drink = $_POST["drink"];
+    $quantity = $_POST["quantity"];
+    
+    if(trim($name) != ""){
+    echo "<h2> Inventory: </h2>";
+    session_start();
+    if(isset($_SESSION[$name])){
 
+    }else{
+        $_SESSION[$name]["milk"] = 0;
+        $_SESSION[$name]["soda"] = 0;
+    }
+    if(isset($_POST["add"])){
+        $_SESSION[$name][$drink] += $quantity;
+    }else{
+        $_SESSION[$name][$drink] -= $quantity;
+    }
+    
+    echo "worker: $name <br>";
+    echo "units milk: ". $_SESSION[$name]["milk"] ."<br>";
+    echo "units soft drink: ". $_SESSION[$name]["soda"];
+}
+}
 ?>
---
+++ PHP/ExerciseSession02.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+if(!isset($_SESSION["numbers"])){
+    $_SESSION["numbers"] = array(10, 20, 30);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Modify array saved in session </h1>
+    <form method = POST>
+        Postion to modify: 
+    <select name = position>
+        <option value = 0> 0 </option>
+        <option value = 1> 1 </option>
+        <option value = 2> 2 </option>
+    </select>
+    <br> <br>
+    New value:
+    <input type = text name = value>
+    <br> <br>
+    <input type = submit value = Modify name = option>
+    <input type = submit value = Average name = option>
+    <input type = submit value = Reset name = option>
+    </form>
+    <br>
+</body>
+</html>
+
+<?php
+
+
+if(isset($_POST["option"])){
+    $option = $_POST["option"];
+    $position = $_POST["position"];
+    $newValue = $_POST["value"];
+    if($option == "Modify"){
+        $_SESSION["numbers"][$position] = $newValue;
+    }elseif($option == "Average"){
+        echo "<br> Average : ";
+        echo array_sum(($_SESSION["numbers"]))/count(($_SESSION["numbers"]));
+        echo "Current array: ";
+        foreach($_SESSION["numbers"] as $number){
+        echo "$number, ";
+        }
+    }  
+}
+    echo "Current array: ";
+    foreach($_SESSION["numbers"] as $number){
+    echo "$number, ";
+    }
+?>
